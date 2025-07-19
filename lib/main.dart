4ef3de0ca3413dbe1ebac2e39a4c63f8fadb4f65
اسمك هنا <_@example.com>
2025-07-19 13:48:21 +0200

    إنهاء مهمة تعديل الألوان والخطوط ورفع ملف app_colors.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart'; // ← استيراد الصفحة
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Furniture Movers',
+      home: const HomeScreen(), // ← هون حددنا أول صفحة تظهر
     );
   }
 }
