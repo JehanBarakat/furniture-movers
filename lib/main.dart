a68ba7e27cf7b2e53148cc5aa7249064dccef0fc
JehanBarakat <gehanbr475@gmail.com>
2025-07-22 16:45:33 +0300

    add nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,60 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(  title: 'Furniture Movers', ); }}
+
 import 'package:flutter/material.dart';
-import 'forgetPassword/resetPassword.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furnituremovers/core/constants/app_colors.dart';
+import 'package:furnituremovers/core/widgets/custom_bottom_nav_bar.dart'; 
 
 void main() {
   runApp(const MyApp());
+}
+
+// Separate pages
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('الرئيسية'));
+  }
+}
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('الشات'));
+  }
+}
+
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('المفضلة'));
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('البروفايل'));
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,22 +64,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Furniture Movers',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white, // ✅ يجعل خلفية كل الشاشات بيضاء
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
-          titleTextStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: Colors.black,
-            fontFamily: 'Roboto',
-          ),
-        ),
+        fontFamily: 'Almarai',
+        useMaterial3: true, // Recommended for modern Flutter apps
       ),
-      home: const ResetPasswordScreen(), // ✅ عرض الشاشة الرئيسية
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = const [
+    HomePage(),
+    ChatPage(),
+    FavoritePage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
