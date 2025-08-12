79e7cb3a26384690a0186e62486bb342de8dbf3e
Jehan <gehanbr475@gmail.com>
2025-08-12 22:37:30 +0300

    Update login_screen.dart

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -9,6 +9,7 @@ import 'package:furnituremovers/core/widgets/custom_phone_field.dart';
 import 'package:furnituremovers/core/widgets/custom_text_field.dart';
 import 'package:furnituremovers/core/widgets/primary_button.dart';
 import 'package:furnituremovers/features/auth/presentation/screens/register_screen.dart';
+import 'package:furnituremovers/features/auth/presentation/screens/forget_password_screen.dart';
 import 'package:furnituremovers/main.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -59,26 +60,29 @@ class _LoginScreenState extends State<LoginScreen> {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         backgroundColor: AppColors.white,
+        resizeToAvoidBottomInset: true, // إضافة هذا السطر
         body: Stack(
           children: [
             SafeArea(
               child: LayoutBuilder(
                 builder: (context, constraints) {
-                  return Padding(
-                    padding: AppSpacing.screenPadding,
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                        minHeight: constraints.maxHeight,
-                      ),
-                      child: IntrinsicHeight(
+                  return SingleChildScrollView(
+                    // إضافة SingleChildScrollView
+                    child: Padding(
+                      padding: AppSpacing.screenPadding,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: constraints.maxHeight,
+                        ),
                         child: Column(
+                          // إزالة IntrinsicHeight
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             // الترحيب
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
-                                SizedBox(height: 50.h),
+                                SizedBox(height: 30.h), // تقليل المسافة
                                 Text(
                                   "مرحباً بعودتك مجددًا...",
                                   style: AppTextStyles.heading1Ar,
@@ -98,8 +102,11 @@ class _LoginScreenState extends State<LoginScreen> {
                             SizedBox(height: 16.h),
 
                             // الصورة
-                            Expanded(
-                              flex: 2,
+                            Container(
+                              // تغيير من Expanded إلى Container
+                              height:
+                                  MediaQuery.of(context).size.height *
+                                  0.25, // تحديد ارتفاع ثابت
                               child: Center(
                                 child: Image.asset(
                                   "assets/images/login-image.png",
@@ -111,82 +118,89 @@ class _LoginScreenState extends State<LoginScreen> {
                             SizedBox(height: 16.h),
 
                             // الحقول
-                            Expanded(
-                              flex: 3,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    "رقم التلفون",
-                                    style: AppTextStyles.buttonTextAr.copyWith(
-                                      color: AppColors.black,
-                                    ),
+                            Column(
+                              // تغيير من Expanded إلى Column عادي
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "رقم التلفون",
+                                  style: AppTextStyles.buttonTextAr.copyWith(
+                                    color: AppColors.black,
                                   ),
-                                  SizedBox(
-                                    height: AppSpacing.mediumVerticalSpacing.h,
-                                  ),
-                                  CustomPhoneInput(onChanged: (fullNumber) {}),
+                                ),
+                                SizedBox(
+                                  height: AppSpacing.mediumVerticalSpacing.h,
+                                ),
+                                CustomPhoneInput(onChanged: (fullNumber) {}),
 
-                                  SizedBox(height: 16.h),
+                                SizedBox(height: 16.h),
 
-                                  Text(
-                                    "كلمة المرور",
-                                    style: AppTextStyles.buttonTextAr.copyWith(
-                                      color: AppColors.black,
+                                Text(
+                                  "كلمة المرور",
+                                  style: AppTextStyles.buttonTextAr.copyWith(
+                                    color: AppColors.black,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: AppSpacing.mediumVerticalSpacing.h,
+                                ),
+                                CustomTextField(
+                                  textDirection: TextDirection.rtl,
+                                  hintText: '*****************',
+                                  controller: passwordController,
+                                  obscureText: true,
+                                  prefixIcon: SvgPicture.asset(
+                                    'assets/icons/lock-open.svg',
+                                    width: 14,
+                                    height: 19,
+                                    colorFilter: ColorFilter.mode(
+                                      AppColors.mediumPrimary,
+                                      BlendMode.srcIn,
                                     ),
                                   ),
-                                  SizedBox(
-                                    height: AppSpacing.mediumVerticalSpacing.h,
-                                  ),
-                                  CustomTextField(
-                                    textDirection: TextDirection.rtl,
-                                    hintText: '*****************',
-                                    controller: passwordController,
-                                    obscureText: true,
-                                    prefixIcon: SvgPicture.asset(
-                                      'assets/icons/lock-open.svg',
-                                      width: 14,
-                                      height: 19,
-                                      colorFilter: ColorFilter.mode(
-                                        AppColors.mediumPrimary,
-                                        BlendMode.srcIn,
-                                      ),
-                                    ),
-                                    validator: (value) {
-                                      if (value == null ||
-                                          value.trim().isEmpty) {
-                                        return 'الرجاء إدخال كلمة المرور';
-                                      }
-                                      if (value.trim().length < 8) {
-                                        return 'كلمة المرور يجب أن تكون على الأقل 8 أحرف';
-                                      }
-                                      return null;
+                                  validator: (value) {
+                                    if (value == null || value.trim().isEmpty) {
+                                      return 'الرجاء إدخال كلمة المرور';
+                                    }
+                                    if (value.trim().length < 8) {
+                                      return 'كلمة المرور يجب أن تكون على الأقل 8 أحرف';
+                                    }
+                                    return null;
+                                  },
+                                ),
+
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      // الانتقال لصفحة نسيت كلمة المرور
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              const ForgetPasswordScreen(),
+                                        ),
+                                      );
                                     },
-                                  ),
-
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: TextButton(
-                                      onPressed: () {},
-                                      child: Text(
-                                        "نسيت كلمة المرور؟",
-                                        style: AppTextStyles.smallTextAr,
-                                      ),
+                                    child: Text(
+                                      "نسيت كلمة المرور؟",
+                                      style: AppTextStyles.smallTextAr,
                                     ),
                                   ),
+                                ),
 
-                                  Spacer(),
+                                SizedBox(height: 20.h), // إضافة مسافة ثابتة
 
-                                  PrimaryButton(
-                                    text: "تسجيل الدخول",
-                                    padding: const EdgeInsets.all(5),
-                                    textStyle: AppTextStyles.bodyTextAr
-                                        .copyWith(color: AppColors.white),
-                                    onTap: _login,
-                                    width: double.infinity,
+                                PrimaryButton(
+                                  text: "تسجيل الدخول",
+                                  padding: const EdgeInsets.all(5),
+                                  textStyle: AppTextStyles.bodyTextAr.copyWith(
+                                    color: AppColors.white,
                                   ),
-                                ],
-                              ),
+                                  onTap: _login,
+                                  width: double.infinity,
+                                ),
+                              ],
                             ),
 
                             // رابط إنشاء حساب
